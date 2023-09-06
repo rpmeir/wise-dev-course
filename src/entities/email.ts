f124dbe3eba9d154a000e4c0f22f0f0ac5b3c691
Rodrigo Pires Meira <rpmeir@gmail.com>
2023-09-06 19:16:26 -0300

    test: make value public

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -3,7 +3,7 @@ import { InvalidEmailError } from "./errors/invalid-email-error";
 
 export class Email {
 
-    private readonly value: string;
+    public readonly value: string;
 
     private constructor (email: string) {
         this.value = email;
