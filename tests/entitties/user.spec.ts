1cf812164a9a48a5844c36ef9a742499d2021562
Rodrigo Pires Meira <rpmeir@gmail.com>
2023-09-06 18:46:09 -0300

    feat: add user class

--- tests/entitties/user.spec.ts
+++ tests/entitties/user.spec.ts
@@ -1,7 +1,11 @@
+import { InvalidEmailError } from "../../src/entities/errors/invalid-email-error";
+import { User } from "../../src/entities/user";
+import { left } from "../../src/shared/either";
+
 describe('User domain entity', () => {
     test('Should not create user with invalid email address', () => {
-        // const invalidEmail = 'invalidEmail';
-        // const error = User.create({name: 'any_name', email: invalidEmail});
-
+        const invalidEmail = 'invalidEmail';
+        const error = User.create({name: 'any_name', email: invalidEmail});
+        expect(error).toEqual(left(new InvalidEmailError()));
     });
 });
