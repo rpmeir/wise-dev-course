3b6d93908627c5201619a56bc341b5207248c6e9
Rodrigo Pires Meira <rpmeir@gmail.com>
2023-09-06 18:14:30 -0300

    refactor: move user data to entities

---
+++ tests/entitties/user.spec.ts
@@ -0,0 +1,7 @@
+describe('User domain entity', () => {
+    test('Should not create user with invalid email address', () => {
+        // const invalidEmail = 'invalidEmail';
+        // const error = User.create({name: 'any_name', email: invalidEmail});
+
+    });
+});
--- tests/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ tests/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,6 +1,6 @@
 import { UserRepository } from "../../../src/usecases/register-user-on-mailing-list/ports/user-repository";
 import { InMemoryUserRepository } from "../../../src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository";
-import { UserData } from "../../../src/usecases/register-user-on-mailing-list/user-data";
+import { UserData } from "../../../src/entities/user-data";
 
 describe('Register user on mailing list use case', () =>{
     test('Should add user with complete data to mailing list', async () => {
