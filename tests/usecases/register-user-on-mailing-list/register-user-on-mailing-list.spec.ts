67f0dd0626962e48187c636d26296e5acc6d1594
Rodrigo Pires Meira <rpmeir@gmail.com>
2023-09-08 19:44:09 -0300

    test: add test to add use in mailing list

--- tests/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ tests/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,20 +1,20 @@
 import { UserRepository } from "../../../src/usecases/register-user-on-mailing-list/ports/user-repository";
 import { InMemoryUserRepository } from "../../../src/usecases/register-user-on-mailing-list/repository/in-memory-user-repository";
 import { UserData } from "../../../src/entities/user-data";
+import { RegisterUserOnMailingList } from "../../../src/usecases/register-user-on-mailing-list/register-user-on-mailing-list";
 
 describe('Register user on mailing list use case', () =>{
+
     test('Should add user with complete data to mailing list', async () => {
         const users: UserData[] = [];
         const repo: UserRepository = new InMemoryUserRepository(users);
         const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
         const name = 'User Name';
         const email = 'user@email.com';
-
         const response = await usecase.registerUserOnMailingList({name, email});
-
         const user = repo.findUserByEmail(email);
-
         expect((await user).name).toBe(name);
-
+        expect(response.value.name).toBe(name);
     });
+
 });
